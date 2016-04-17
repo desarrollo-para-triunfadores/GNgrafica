b1640c5f9bc4ecdc1bf7be14ed23aef8624654c9
Juan Pablo Cáceres <jpaulnava@gmail.com>
2016-04-17 00:37:34 -0300

    Proveedores agregado con request, falta la vista

--- app/Http/Controllers/ProveedoresController.php
+++ app/Http/Controllers/ProveedoresController.php
@@ -3,10 +3,108 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Storage;
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Proveedor;
+use App\Logo_Proveedor;
+use Laracasts\Flash\Flash;
+use App\Http\Requests\ProveedorRequestCreate;
+use App\Http\Requests\ProveedorRequestEdit;
+use Carbon\Carbon;
+use Illuminate\Routing\Route;
 
 class ProveedoresController extends Controller
 {
-    //
+    public function __construct()
+    {
+    	Carbon::setlocale('es'); 	// Instancio en Español el manejador de fechas de Laravel
+        
+    }
+
+    public function index()
+    {     
+        $proveedores = Proveedor::searchNombres($request->nombre)
+        ->searchOrigen($request->idorigen)
+        ->searchRubro($request->idrubro)
+        ->orderBy('id','ASC')
+        ->paginate();
+        if($request->ajax()){ 	//Si la solicitud fue realizada utilizando ajax se devuelven los registros únicamente a la tabla.
+            return response()->json(view('admin.proveedores.tablaLogos',compact('proveedores'))->render());
+        }
+        return view('admin.proveedores.index')->with('proveedores',$proveedores);        
+    }
+
+
+    public function store(ProveedorRequestCreate $request)
+    {
+        $proveedor = new Proveedor($request->all());
+        $proveedor->save();
+
+        //Manipulación de Imágenes...
+        $nombreImagen = 'sin imagen';
+
+        if ($request->file('imagen'))
+        {
+            $file = $request->file('imagen');        
+            $nombreImagen = 'GN_' . time() . '.' . $file->getClientOriginalExtension();
+            Storage::disk('proveedores')->put($nombreImagen, \File::get($file));
+        }        
+
+        $imagen = new Logo_Proveedor();
+        $imagen->nombre = $nombreImagen;
+        $imagen->proveedor()->associate($proveedor);
+        $imagen->save();
+
+        Flash::success('El proveedor "'. $proveedor->nombre.'"" ha sido registrado de forma exitosa.');
+        return redirect()->route('admin.proveedores.index');
+    }
+
+
+    public function show($id)
+    {
+    	$proveedor = Proveedor::find($id);
+        return view('admin.proveedores.show')->with('proveedor', $proovedor);
+    }
+
+
+
+    public function update(ProveedorRequestEdit $request, $id)
+    {
+    	$proveedor = Proveedor::find($id);
+
+        if ($request->file('imagen'))
+        {                  
+            $logo_proveedor = $proveedor->logo_proveedor;
+            $file = $request->file('imagen');        
+            $nombreImagen = 'GN_' . time() . '.' . $file->getClientOriginalExtension();            
+            if (Storage::disk('proveedores')->exists($logo_proveedor->nombre))
+             {
+                Storage::disk('proveedores')->delete($logo_proveedor->nombre);   // Borramos la imagen anterior.      
+             }
+            $logo_proveedor->nombre = $nombreImagen;  // Actualizamos el nombre de la nueva imagen.
+            $logo_proveedor->save();           
+            Storage::disk('proveedores')->put($nombreImagen, \File::get($file));  // Movemos la imagen nueva al directorio /imagenes/empresas   
+        }  
+
+        $proveedor->fill($request->all());
+        $proveedor->save();
+        Flash::success("Se ha realizado la actualización del registro: ".$proveedor->nombre.".");
+        return redirect()->route('admin.proveedor.show', $id);
+    }
+
+    public function destroy($id)
+    {
+    	$proveedor = Proveedor::find($id);
+
+        $logo_proveedor = $proveedor->logo_proveedor;
+        if ($logo_proveedor->nombre != 'sin imagen')
+        {            
+            Storage::disk('proveedores')->delete($logo_empresa->nombre); // Borramos la imagen asociada.
+        } 
+        $proveedor->delete();        
+        Flash::error("Se ha realizado la eliminación del registro: ".$proveedor->nombre.".");        
+        return redirect()->route('admin.proveedores.index');
+    }
+
 }
---
+++ app/Http/Requests/ProveedoresRequestEdit.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class ProveedoresRequestEdit extends Request
+{
+    public function __construct(Route $route)
+    {
+        $this->route = $route;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [            
+            'nombre' => 'required|max:100|unique:proveedores,nombre,'.$this->route->getParameter('proveedores'),
+            'celular' => 'max:30',
+            'telefono' => 'max:30',
+            'calle' => 'required|max:50',
+            'altura' => 'required|max:10',
+            'localidad_id' => 'required',
+            'rubro_id' => 'required',
+            'email' => 'email|max:100',
+            'web'  => 'active_url|max:100',
+            'imagen' => 'mimes:jpeg,png|max:3072'
+
+            'horario_atencion' => 'horario_atencion'     
+        ];      
+    }
+}
